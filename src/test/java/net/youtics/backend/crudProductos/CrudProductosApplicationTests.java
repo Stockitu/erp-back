57bd018954903d102257abfe637e4d4861fe5f2f
Jazmin Aliaga <110360566+jazminaliaga@users.noreply.github.com>
2025-05-20 11:42:28 -0300

    Pruebas de Integración con Spring Boot Test

--- src/test/java/net/youtics/backend/crudProductos/CrudProductosApplicationTests.java
+++ src/test/java/net/youtics/backend/crudProductos/CrudProductosApplicationTests.java
@@ -1,14 +1,38 @@
 package net.youtics.backend.crudProductos;
 
-import org.junit.jupiter.api.Disabled;
+import net.youtics.backend.crudProductos.entities.Producto;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-@Disabled("Desactivado temporalmente por error de configuración")
-@SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CrudProductosApplicationTests {
+
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
 	void contextLoads() {
+
 	}
 
+	@Test
+	void testGetAllProductos() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/api/productos", String.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getBody()).contains("nombre");
+	}
+
+	@Test
+	void testCreateProducto() {
+		Producto producto = new Producto(null, "Dulce", 10, "MarcaX", "Dulce de prueba", 1500.0);
+
+		ResponseEntity<Producto> response = restTemplate.postForEntity("/api/productos", producto, Producto.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+		assertThat(response.getBody().getNombre()).isEqualTo("Dulce");
+	}
 }
